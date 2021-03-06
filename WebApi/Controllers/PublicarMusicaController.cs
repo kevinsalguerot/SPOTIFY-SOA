1d338166e30bec1f1969d41402caa2ff921cc5be
Kevin <kevinsalguero.t@gmail.com>
2021-03-06 11:02:24 -0500

    Consume

---
+++ WebApi/Controllers/PublicarMusicaController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebApi.Models;
+using WebApi.Transfers;
+
+namespace WebApi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class PublicarMusicaController : ApiController
+    {
+        [HttpGet]
+        [Route("api/PublicarMusica/ObtenerMusica")]
+        public musicadto ObtenerMusica(int id)
+        {
+            return Musica.ObtenerMusica(id);
+        }
+        [HttpPut]
+        [Route("api/PublicarMusica/RegistraMusica")]
+        public musicadto RegistrarAlumno(musicadto alumnodto)
+        {
+            return Musica.RegistrarMusica(alumnodto);
+        }
+        [HttpPost]
+        [Route("api/AfiliacionUsuarios/ActualizarMusica")]
+        public musicadto ActualizarAlumno(int id, musicadto alumnodto)
+        {
+            return Musica.ActualizarMusica(id, alumnodto);
+        }
+    }
+}
--- WebApi/Models/musicaspotify.cs
+++ WebApi/Models/musicaspotify.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using WebApi.Transfers;
@@ -25,6 +26,51 @@ namespace WebApi.Models
                        };
 
             return list;
+        }
+
+
+        public static musicadto ObtenerMusica(int id)
+        {
+            spotifyEntities db = new spotifyEntities();
+            var obj = db.Musicas.Select(b =>
+                    new musicadto()
+                    {
+                        Id = b.Id,
+                        Titulo = b.Titulo,
+                        Artista = b.Artista,
+                        Año = b.Año,
+                        Archivo = b.Archivo,
+                        IdAlbum = b.IdAlbum
+                    }).SingleOrDefault(b => b.Id == id);
+            if (obj == null) obj = new musicadto() { Id = 0, Titulo = "", Artista = "", Año = null, Archivo = "", IdAlbum = 0 };
+            return obj;
+        }
+        public static musicadto RegistrarMusica(musicadto musicadto)
+        {
+            spotifyEntities db = new spotifyEntities();
+            Musica musica = new Musica()
+            {
+                           Titulo = musicadto.Titulo,
+                           Artista = musicadto.Artista,
+                           Año = musicadto.Año,
+                           Archivo = musicadto.Archivo,
+                           IdAlbum = musicadto.IdAlbum
+            };
+            db.Musicas.Add(musica);
+            db.SaveChanges();
+            return Musica.ObtenerMusica(musica.Id);
+        }
+
+        public static musicadto ActualizarMusica(int id,musicadto musicadto)
+        {
+            spotifyEntities db = new spotifyEntities();
+            Musica musica = db.Musicas.Find(id);
+            musica.Titulo = musicadto.Titulo;
+            musica.Artista = musicadto.Artista;
+            musica.Año = musicadto.Año;
+            db.Entry(musica).State = EntityState.Modified;
+            db.SaveChanges();
+            return Musica.ObtenerMusica(musica.Id);
         }
     }
 }
